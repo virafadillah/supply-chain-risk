b45fc0e611f0d80a5fb5551a1071c5bb4d077a42
Khairul122 <khairulhuda242@gmail.com>
2026-07-29 09:54:07 +0700

    feat: add UserSeeder to populate initial admin and standard user accounts

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Schema;
 
 class UserSeeder extends Seeder
 {
@@ -13,26 +14,36 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        $hasIsAdmin = Schema::hasColumn('users', 'is_admin');
+
         // Account Admin
+        $adminData = [
+            'name' => 'Administrator',
+            'password' => Hash::make('password123'),
+            'email_verified_at' => now(),
+        ];
+        if ($hasIsAdmin) {
+            $adminData['is_admin'] = true;
+        }
+
         User::updateOrCreate(
             ['email' => 'admin@gmail.com'],
-            [
-                'name' => 'Administrator',
-                'password' => Hash::make('password123'),
-                'is_admin' => true,
-                'email_verified_at' => now(),
-            ]
+            $adminData
         );
 
         // Account User Biasa
+        $userData = [
+            'name' => 'User Biasa',
+            'password' => Hash::make('password123'),
+            'email_verified_at' => now(),
+        ];
+        if ($hasIsAdmin) {
+            $userData['is_admin'] = false;
+        }
+
         User::updateOrCreate(
             ['email' => 'user@gmail.com'],
-            [
-                'name' => 'User Biasa',
-                'password' => Hash::make('password123'),
-                'is_admin' => false,
-                'email_verified_at' => now(),
-            ]
+            $userData
         );
     }
 }
